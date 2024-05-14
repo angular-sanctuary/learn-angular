eba8609d24edd64ea79eefdf8992f69b6d2b826c
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-05-14 21:28:12 +0200

    feat: add zoom

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import starlightImageZoom from 'starlight-image-zoom';
 
 // https://astro.build/config
 export default defineConfig({
@@ -179,6 +180,9 @@ export default defineConfig({
                 },
             },
             customCss: ['./src/tailwind.css'],
+            plugins: [
+                starlightImageZoom(),
+            ]
         }),
         tailwind({applyBaseStyles: false}),
     ],
