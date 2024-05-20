a28a62b1ec6dd9c963f862d2315a7255f1f8c8fb
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-05-20 16:20:53 +0200

    feat: quizz

---
+++ src/pages/quiz/data/version-intermediate.ts
@@ -0,0 +1,251 @@
+import type {Quiz} from "../quiz.model.ts";
+
+export const versionIntermediateQuiz: Quiz = {
+    id: 1,
+    title: "Find out the right release version of an Angular feature",
+    questions: [
+        {
+            id: 1,
+            title: "Which Angular version introduced the Ivy renderer?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 8",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 9",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 10",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 11",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 11 introduced the Ivy renderer as the default rendering engine."
+        },
+        {
+            id: 2,
+            title: "Which Angular version introduced the Angular CLI?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 1",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 6",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 6 introduced the Angular CLI as a command-line interface for Angular."
+        },
+        {
+            id: 3,
+            title: "Which Angular version introduced the Angular Router?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 1",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 6",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 6 introduced the Angular Router as a powerful routing library for Angular."
+        },
+        {
+            id: 4,
+            title: "Which Angular version introduced the Angular Forms?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 1",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 6",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 6 introduced the Angular Forms as a powerful way to manage forms in Angular."
+        },
+        {
+            id: 5,
+            title: "Which Angular version introduced the Angular HttpClient?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 1",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 6",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 6 introduced the Angular HttpClient as a powerful way to make HTTP requests in Angular."
+        },
+        {
+            id: 6,
+            title: "Which Angular version introduced the Angular Material?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 6",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 7",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 7 introduced the Angular Material as a set of UI components for Angular."
+        },
+        {
+            id: 7,
+            title: "Which Angular version introduced the Angular Elements?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 6",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 8",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 8 introduced the Angular Elements as a way to use Angular components in non-Angular environments."
+        },
+        {
+            id: 8,
+            title: "Which Angular version introduced the Angular PWA?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 6",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 7",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 7 introduced the Angular PWA as a way to build Progressive Web Apps in Angular."
+        },
+        {
+            id: 9,
+            title: "Which Angular version introduced the Angular Universal?",
+            options: [
+                {
+                    id: 1,
+                    title: "Angular 2",
+                    isCorrect: false
+                },
+                {
+                    id: 2,
+                    title: "Angular 4",
+                    isCorrect: false
+                },
+                {
+                    id: 3,
+                    title: "Angular 6",
+                    isCorrect: false
+                },
+                {
+                    id: 4,
+                    title: "Angular 7",
+                    isCorrect: true
+                }
+            ],
+            answerExplanation: "Angular 7 introduced the Angular Universal as a way to build server-side rendered apps in Angular."
+        },
+    ]
+}
